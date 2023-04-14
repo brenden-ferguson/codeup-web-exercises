88d812f9260f76246ea0e17e04dfe9a647836cd3
Brenden <brenden.m.ferguson@gmail.com>
2023-04-14 10:33:06 -0500

    Added prompt/alert for movie rental question

--- js/external.js
+++ js/external.js
@@ -1,4 +1,21 @@
+//greeting to the website in the console
+
 console.log('Hello from External JavaScript.');
+
+//pop-up when first loading the site
+
 alert('Welcome to my Website!');
+
+//asks for a favorite color and responds stating that it is also my favorite
+
 var favoriteColor = prompt('What is you favorite color?');
-    alert(favoriteColor + ' is my favorite color too!')
+    alert(favoriteColor.trim() + ' is my favorite color too!');
+
+//completing steps for JavaScript with HTML exercise
+
+var littleMermaid = prompt('How many days would you like to rent The Little Mermaid?');
+var brotherBear = prompt('How many days would you like to rent Brother Bear?');
+var hercules = prompt('How many days would you like to rent Hercules?');
+
+    alert('Because the rental price of each movie is $3.00 per day, you total will be $' +
+        ((littleMermaid * 3) + (brotherBear * 3) + (hercules * 3)) + '.00.');
