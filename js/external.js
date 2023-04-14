58d688c02ac36520e1021e57a4f41438b1d2845b
Brenden <brenden.m.ferguson@gmail.com>
2023-04-14 10:13:03 -0500

    Added questions to external and added greeting to inline

---
+++ js/external.js
@@ -0,0 +1,4 @@
+console.log('Hello from External JavaScript.');
+alert('Welcome to my Website!');
+var favoriteColor = prompt('What is you favorite color?');
+    alert(favoriteColor + ' is my favorite color too!')
