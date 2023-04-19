4640304f72f285bff7ff5d8550397936ba697203
Brenden <brenden.m.ferguson@gmail.com>
2023-04-19 17:10:59 -0500

    Solved problem 3; Need explanation on how

--- js/for_loops.js
+++ js/for_loops.js
@@ -27,11 +27,14 @@
 
 
     function pyramid() {
+        let string = "";
         for (let i = 1; i < 10; i++) {
             for(let y = 1; y <= i; y++ ){
-
+                string += i;
             }
+            string += "\n";
         }
+        console.log(string);
     }
 pyramid();
 })();
