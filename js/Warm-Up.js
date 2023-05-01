e267c504e5b5c3ba768792fe830ccd80adbc24ba
Brenden <brenden.m.ferguson@gmail.com>
2023-05-01 14:12:15 -0500

    Changed some spacing

--- js/Warm-Up.js
+++ js/Warm-Up.js
@@ -1,3 +1,17 @@
-function seven(){
+function seven() {
     return 7;
+}
+
+function findFactors(input) {
+    let factorArray = [];
+    if (typeof input === "number" && !isNaN(input)) {
+        for (i = 1; i <= input; i++) {
+            if (input % i === 0) {
+                factorArray.push(i);
+            }
+        }
+        return factorArray;
+    } else {
+        return false;
+    }
 }
