a4cfcffc50acac2217a1ce4cbbc9c546efaa72c3
Brenden <brenden.m.ferguson@gmail.com>
2023-05-04 10:43:15 -0500

    Initial commit of Positioning exercise

--- js/Warm-Up.js
+++ js/Warm-Up.js
@@ -35,3 +35,17 @@ function countEs(str) {
 function hasEs(str){
     return countEs(str) > 0;
 }
+
+function fizzBuzz(){
+    for(i=1; i<=100; i++){
+        if(i%3===0 && i%5===0){
+            console.log('fizzbuzz');
+        } else if(i%5===0){
+            console.log('buzz');
+        } else if(i%3===0){
+            console.log('fizz');
+        } else{
+            console.log(i);
+        }
+    }
+}
