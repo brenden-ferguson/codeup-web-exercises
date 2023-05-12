a3d982a090fce596a316520be0759e8dbf763d8a
Brenden <brenden.m.ferguson@gmail.com>
2023-05-12 09:30:10 -0500

    Completed extractNames exercise

--- js/Warm-Up.js
+++ js/Warm-Up.js
@@ -73,3 +73,45 @@ function explainString(str) {
         isEvenLength: str.length % 2 === 0
     }
 }
+
+const hamsters = [
+    {
+        name: "Hamtaro",
+        heightInMM: 86,
+        fur: ['orange', 'white'],
+        gender: "male",
+        dateOfBirth: "August 6"
+    } , {
+        name: "Bijou",
+        heightInMM: 75,
+        fur: ['white'],
+        gender: "female",
+        dateOfBirth: "July 10"
+    } , {
+        name: "Oxnard",
+        heightInMM: 100,
+        fur: ['grey', 'white'],
+        gender: "male",
+        dateOfBirth: "May 3"
+    } , {
+        name: "Boss",
+        heightInMM: 120,
+        fur: ['brown', 'white'],
+        gender: "male",
+        dateOfBirth: "September 21"
+    } , {
+        name: "Snoozer",
+        heightInMM: 85,
+        fur: ['brown', 'white', "pink"],
+        gender: "male",
+        dateOfBirth: "January 14"
+    }
+];
+
+function extractNames(arr){
+    let bucket = [];
+    arr.forEach(function(el){
+        bucket.push(el.name);
+    })
+    return bucket;
+}
