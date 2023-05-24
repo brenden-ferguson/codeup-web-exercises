ba88ea65ba7613e967f04e06859a1a32211c3633
Brenden <brenden.m.ferguson@gmail.com>
2023-05-24 15:17:29 -0500

    Completed jquery faq exercise

---
+++ js/jquery_faq.js
@@ -0,0 +1,95 @@
+"use strict";
+
+$(function(){
+
+
+// $('dd').addClass('invisible', 'noShow');
+
+// $('.invisible').css('visibility', 'hidden')
+
+// $('#dt-1').on('click', function (){
+//     $('.noShow-1').toggleClass('invisible')
+// })
+//
+// $('#dt-2').on('click', function (){
+//     $('.noShow-2').toggleClass('invisible')
+// })
+// $('#dt-3').on('click', function (){
+//     $('.noShow-3').toggleClass('invisible')
+// })
+// $('#dt-4').on('click', function (){
+//     $('.noShow-4').toggleClass('invisible')
+// })
+// $('#dt-5').on('click', function (){
+//     $('.noShow-5').toggleClass('invisible')
+// })
+// $('#dt-6').on('click', function (){
+//     $('.noShow-6').toggleClass('invisible')
+// })
+// $('#dt-7').on('click', function (){
+//     $('.noShow-7').toggleClass('invisible')
+// })
+// $('#dt-8').on('click', function (){
+//     $('.noShow-8').toggleClass('invisible')
+// })
+// $('#dt-9').on('click', function (){
+//     $('.noShow-9').toggleClass('invisible')
+// })
+// $('#dt-10').on('click', function (){
+//     $('.noShow-10').toggleClass('invisible')
+// })
+
+
+
+
+$('dd').css('display', 'none');
+
+
+$('#dt-1').on('click', function(){
+    console.log('Event Fire')
+    $('.noShow-1').toggleClass('invisible').toggle();
+});
+
+$('#dt-2').on('click', function(){
+    console.log('Event Fire')
+    $('.noShow-2').toggleClass('invisible').toggle();
+});
+
+$('#dt-3').on('click', function(){
+    console.log('Event Fire')
+    $('.noShow-3').toggleClass('invisible').toggle();
+});
+
+$('#dt-4').on('click', function(){
+    console.log('Event Fire')
+    $('.noShow-4').toggleClass('invisible').toggle();
+});
+
+$('#dt-5').on('click', function(){
+    console.log('Event Fire')
+    $('.noShow-5').toggleClass('invisible').toggle();
+});
+
+$('#dt-6').on('click', function(){
+    console.log('Event Fire')
+    $('.noShow-6').toggleClass('invisible').toggle();
+});
+
+$('#dt-7').on('click', function(){
+    console.log('Event Fire')
+    $('.noShow-7').toggleClass('invisible').toggle();
+});
+
+$('#dt-8').on('click', function(){
+    $('.noShow-8').toggleClass('invisible').toggle();
+});
+
+$('#dt-9').on('click', function(){
+    $('.noShow-9').toggleClass('invisible').toggle();
+});
+
+$('#dt-10').on('click', function(){
+    $('.noShow-10').toggleClass('invisible').toggle();
+});
+
+});
