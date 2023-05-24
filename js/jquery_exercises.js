ab61bb1fb177a41e7df6df059452b867228a5e75
Brenden <brenden.m.ferguson@gmail.com>
2023-05-23 20:45:52 -0500

    Made konami code in js file

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,11 +1,34 @@
 "use strict";
-$(function (){
+$(function () {
 
     $('li').css('font-size', '20px');
 
     $('h1, p, li').css('background-color', 'yellow');
 
-    let contents=$('h1').html();
+    let contents = $('h1').html();
     alert(contents);
+
+    let konami = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a', 'Enter']
+    let userCode = [];
+    let i = userCode.length;
+    $(document).on('keyup', function(e){
+        userCode.push(e.key);
+
+        console.log(userCode.length);
+        console.log(i);
+        console.log(userCode.at(i));
+        checkCode();
+    })
+        function checkCode(){
+            if(userCode.at(i) !== konami.at(i)){
+                userCode.splice(0, userCode.length);
+                i = userCode.length - 1
+            } else if((userCode.length === konami.length) && (userCode.at(10) === konami.at(10))){
+                alert('Plus 30 Lives!');
+                $('.k-code').text('33')
+            }
+            i++;
+        }
+
 
 });
