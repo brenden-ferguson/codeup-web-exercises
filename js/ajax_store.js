1b981b57f4189baeb7293e7daf3ec8c4a61ca2fe
Brenden <brenden.m.ferguson@gmail.com>
2023-05-25 16:58:06 -0500

    Adjusted styling and removed unnecessary code

--- js/ajax_store.js
+++ js/ajax_store.js
@@ -1,7 +1,6 @@
 (function () {
     "use strict";
 
-    // TODO: Create an AJAX GET request for the file under data/inventory.json
     $(document).on('ready',searchInventory);
     $("#submitBtn").on('click', searchInventory)
 
@@ -11,11 +10,11 @@
             let html = '';
             data.forEach(function (obj) {
                     html +=
-                        '<tr>' +
-                        '<td>' + obj.title + '</td>' +
-                        '<td>' + obj.quantity + '</td>' +
-                        '<td>' + obj.price + '</td>' +
-                        '<td>' + obj.categories + '</td>' +
+                        '<tr class="border border-2">' +
+                        '<td class="border border-1">' + obj.title + '</td>' +
+                        '<td class="border border-1">' + obj.quantity + '</td>' +
+                        '<td class="border border-1">' + obj.price + '</td>' +
+                        '<td class="border-border-1">' + obj.categories + '</td>' +
                         '</tr>'
                 }
             )
@@ -23,9 +22,5 @@
                 html
             )
         })
-        // TODO: Take the data from inventory.json and append it to the products table
-        //       HINT: Your data should come back as a JSON object; use console.log() to inspect
-        //             its contents and fields
-        //       HINT: You will want to target #insertProducts for your new HTML elements
     }
 })();
