1edfa5fb901a32e5a67ea8ea15ac28ad82b5e135
Brenden <brenden.m.ferguson@gmail.com>
2023-05-30 15:24:00 -0500

    Completed mapbox api exercise with bonus zoom buttons

--- js/mapbox-geocoder-utils.js
+++ js/mapbox-geocoder-utils.js
@@ -17,10 +17,10 @@ function geocode(search, token) {
     var baseUrl = 'https://api.mapbox.com';
     var endPoint = '/geocoding/v5/mapbox.places/';
     return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
-        .then(function(res) {
+        .then(function (res) {
             return res.json();
             // to get all the data from the request, comment out the following three lines...
-        }).then(function(data) {
+        }).then(function (data) {
             return data.features[0].center;
         });
 }
@@ -43,11 +43,62 @@ function reverseGeocode(coordinates, token) {
     var baseUrl = 'https://api.mapbox.com';
     var endPoint = '/geocoding/v5/mapbox.places/';
     return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
-        .then(function(res) {
+        .then(function (res) {
             return res.json();
         })
         // to get all the data from the request, comment out the following three lines...
-        .then(function(data) {
+        .then(function (data) {
             return data.features[0].place_name;
         });
 }
+
+function placeMarkerAndPopup(info, token, map) {
+    geocode(info.address, token).then(function (coordinates) {
+        var popup = new mapboxgl.Popup()
+            .setHTML(info.popupHTML);
+        var marker = new mapboxgl.Marker()
+            .setLngLat(coordinates)
+            .addTo(map)
+            .setPopup(popup);
+        marker.on('mouseenter', function () {
+            popup.addTo(map);
+        })
+    });
+}
+
+let favoriteRestaurants = [
+    {
+        name: "Hugo\'s Cellar",
+        address: "202 Fremont Street Experience, Las Vegas, NV 89101",
+        popupHTML: "<p>Hugo's Cellar</p>"
+    }, {
+        name: "Red Lobster",
+        address: "4233 South Cooper Street, Parks at Arlington Mall Near, Arlington, TX 76015",
+        popupHTML: "<p>Red Lobster</p>"
+    }, {
+        name: "Lazy Dog",
+        address: "241 E Interstate 20, Arlington, TX 76018",
+        popupHTML: "<p>Lazy Dog</p>"
+    }
+]
+
+let zoomAmount = 10;
+
+let zoomPlus = document.querySelector('#zoomUp');
+zoomPlus.addEventListener('click', function () {
+    if (zoomAmount < 25) {
+        zoomAmount += 5;
+        map.setZoom(zoomAmount);
+    }
+    console.log(zoomAmount)
+});
+
+let zoomMinus = document.querySelector('#zoomDown');
+
+zoomMinus.addEventListener('click', function () {
+    if (zoomAmount > 0) {
+        zoomAmount -= 5;
+        map.setZoom(zoomAmount);
+    }
+    console.log(zoomAmount);
+})
