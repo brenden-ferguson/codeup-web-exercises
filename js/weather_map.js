2983f72fd73906d2ead8f2f8dd83d7d339424524
Brenden <brenden.m.ferguson@gmail.com>
2023-06-06 09:16:18 -0500

    Adjusted more spacing

--- js/weather_map.js
+++ js/weather_map.js
@@ -48,19 +48,15 @@
 
     function displayInfo() {
         $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${latLong[0]}&lon=${latLong[1]}&units=imperial&appid=${OWM_TOKEN}`).done(function (data) {
-            console.log(data);
             map.flyTo({
                 center: [latLong[1], latLong[0]],
                 essential: true
             })
             marker.setLngLat([latLong[1], latLong[0]]);
             let daysArr = [];
-            let weatherHTML = '';
-            let currentCity = data.city.name;
+
             for (let i = 0; i < 39; i += 8) {
-
                 // pushes weather info into an array to use for cards
-
                 daysArr.push({
                     day: new Date(data.list[i].dt * 1000),
                     tempLow: data.list[i].main.temp_min,
@@ -71,9 +67,12 @@
             }
 
             // adds current city into the header div
+            let currentCity = data.city.name;
             $('.currentCity').html(currentCity)
 
             // stores weather info into a variable for the cards
+
+            let weatherHTML = '';
             function renderDays(daysArr) {
                 for (let i = 0; i < daysArr.length; i++) {
                     weatherHTML += showDays(daysArr[i]);
