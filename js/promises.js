6716390910bc9eea7d28a168573bcbca9df2a886
Brenden <brenden.m.ferguson@gmail.com>
2023-06-08 11:50:57 -0500

    Removed keys file

--- js/promises.js
+++ js/promises.js
@@ -2,11 +2,11 @@ fetch('https://api.github.com/users')
     .then(response => console.log(response))
     .catch(error => console.error(error));
 
-fetch('https://api.github.com/users/brenden-ferguson/events').then( response => {
-    response.json().then( user => {
-            console.log(user[0].created_at);
-    });
-});
+// fetch('https://api.github.com/users/brenden-ferguson/events').then( response => {
+//     response.json().then(user => {
+//             console.log(user[0].created_at);
+//     });
+// });
 
 function wait(num){
 
@@ -18,3 +18,10 @@ function wait(num){
 }
 wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+
+// ----WalkThrough----
+
+fetch("https://api.github.com/users/brenden-ferguson/events/public", {headers:{'Authorization': `token ${G_TOKEN}`}})
+    .then(response => response.json())
+
