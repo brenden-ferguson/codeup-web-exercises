4033216608ea1ee442a8290d0227bcd546fcdbe7
Brenden <brenden.m.ferguson@gmail.com>
2023-06-14 17:50:39 -0500

    Updated names of files and refactored

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,28 +1,20 @@
 "use strict";
 $(function () {
-
-    $('li').css('font-size', '20px');
-
+    $('p').on('dblclick', size => {
+            $('p').css('font-size', '20px');
+        }
+    )
     $('h1, p, li').css('background-color', 'yellow');
 
-    let contents = $('h1').html();
-    alert(contents);
-
-    let konami = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a', 'Enter']
-    let userCode = [];
-    let i = userCode.length;
-    $(document).on('keyup', checkCode)
-        function checkCode(e){
-            userCode.push(e.key);
-            console.log(userCode);
-            if(userCode.at(i) !== konami.at(i)){
-                userCode.splice(0, userCode.length);
-                i = userCode.length - 1
-            } else if((userCode.length === konami.length) && (userCode.at(10) === konami.at(10))){
-                alert('Plus 30 Lives!');
-                $('.k-code').text('33')
-            }
-            i++;
+    $('h1').on('click', blue => {
+            $('h1').css('background-color', 'blue');
         }
+    )
+
+    $('li').on('mouseenter', highlight => {
+        $('li').css('background-color', 'red');
+    }).on('mouseleave', nohighlight => {
+        $('li').css('background-color', 'yellow');
+    });
 
 });
